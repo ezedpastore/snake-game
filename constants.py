7cb037275a1ecd2108d4ff20138017ff3619eb59
ezedpastore <ezequiel.pastore@mi.unc.edu.ar.com>
2026-03-02 18:54:31 -0300

    feat: constantes
    constantes utilizadas en el proyecto

---
+++ constants.py
@@ -0,0 +1,31 @@
+# colores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_GREEN = (60, 180, 75)
+DARK_GREEN = (20, 120, 40)
+GRAY = (210, 210, 210)
+
+# estados del juego
+STATE_MENU = "menu"
+STATE_SETTINGS = "settings"
+STATE_PLAYING = "playing"
+STATE_COUNTDOWN = "countdown"
+STATE_GAMEOVER = "gameover"
+STATE_EXIT = "exit"
+
+# grid
+GRID_SIZE = 20
+
+# screen dimensions 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# fps
+FPS = 60
+
+# ms
+MOVE_EVENT_INTERVAL = 120  
+
+# countdown seconds
+COUNTDOWN = 3
+
